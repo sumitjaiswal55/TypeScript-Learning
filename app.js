9e68af4751f3c5c3d0419d687fdf53bcb467a06a
sumitjaiswal55 <sj0855530@gmail.com>
2025-09-09 20:58:35 +0530

    Learning day 3

--- app.js
+++ app.js
@@ -1,2 +1,21 @@
-var a = 12;
-console.log(a);
+"use strict";
+// Basic To Advance TypeScript
+Object.defineProperty(exports, "__esModule", { value: true });
+// Datatypes
+// primitive and reference -> [{(these braces are type of refersnces)}]
+// let a = 12;
+// const b = 23;
+// a = "Sumit"
+// Arrays 
+// let arr: number[] = [1,3,4,5,6,7];
+// let arr2 = [1, 2, 3, 4, "Sumit", "Shagun"];
+// tupels
+// let arr2: [number, string] = [12, "Sumit"];
+// Enums (enumerations)
+// enum UserRoles{
+//     ADMIN= "admin",
+//     USER = "user",
+//     SUPER_ADMIN= "superadmin"
+// };
+// UserRoles.SUPER_ADMIN;
+//# sourceMappingURL=app.js.map
